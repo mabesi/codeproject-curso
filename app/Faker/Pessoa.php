39b47aebf26ebf86dc822ccdfbd3b33f7ad8c9f0
Plinio Mabesi <pliniomabesi@gmail.com>
2016-05-27 15:46:20 -0300

    Início do projeto Curso Laravel/Angular

---
+++ app/Faker/Pessoa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CodeProject\Faker;
+use Faker\Provider\pt_BR\Person;
+
+class Pessoa extends Person
+{
+  protected static $titleMale = [];
+  protected static $titleFemale = [];
+}
---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+      echo \CodeProject\Client::all();
+    }
+
+    public function store(Request $request)
+    {
+      return \CodeProject\Client::create($request->all());
+    }
+
+    public function update(Request $request,$id)
+    {
+      \CodeProject\Client::find($id)->update($request->all());
+    }
+
+    public function show($id)
+    {
+      return \CodeProject\Client::find($id);
+    }
+
+    public function delete($id)
+    {
+      \CodeProject\Client::find($id)->delete();
+    }
+
+}
