2253a5d5a11565d4983cb5adb45b1df3af490fb1
Plinio Mabesi <pliniomabesi@gmail.com>
2016-05-30 22:27:11 -0300

    Inclusão de ClientValidator e ClientService

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,15 +3,18 @@
 namespace CodeProject\Http\Controllers;
 
 use CodeProject\Repositories\ClientRepository;
+use CodeProject\Services\ClientService;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
     private $repository;
+    private $service;
 
-    public function __construct(ClientRepository $repository)
+    public function __construct(ClientRepository $repository, ClientService $service)
     {
       $this->repository = $repository;
+      $this->service = $service;
     }
 
     public function index()
@@ -21,22 +24,22 @@ class ClientController extends Controller
 
     public function store(Request $request)
     {
-      return $this->repository->create($request->all());
+      return $this->service->create($request->all());
     }
 
     public function update(Request $request, $id)
     {
-      Client::find($id)->update($request->all());
+      $this->service->update($request->all(),$id);
     }
 
     public function show($id)
     {
-      return Client::find($id);
+      return $this->repository->find($id);
     }
 
     public function delete($id)
     {
-      Client::find($id)->delete();
+      $this->repository->delete($id);
     }
 
 }
