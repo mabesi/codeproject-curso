d848026af29f7605e7c2ee971a578d776f993350
Plinio Mabesi <pliniomabesi@gmail.com>
2016-06-19 16:54:54 -0300

    Instalação e configuração inicial OAUTH2

--- config/app.php
+++ config/app.php
@@ -150,6 +150,8 @@ return [
 
         //Providers Extras
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -207,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
 
     ],
 
